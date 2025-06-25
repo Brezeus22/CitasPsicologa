3526e1519c682b84ebda688d59708f82feece58c
Kevin <kr852042@gmail.com>
2025-06-25 14:05:43 -0400

    Actualizacion

--- emocionvital_db.sql
+++ emocionvital_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-06-2025 a las 19:48:19
+-- Tiempo de generación: 25-06-2025 a las 20:01:50
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
